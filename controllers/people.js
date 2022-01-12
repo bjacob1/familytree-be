e63238b93761414d95bec7797ceb218cb634e9f0
Brian Jacob <brianjacobk@gmail.com>
2022-01-12 12:25:45 -0500

    Working database requests

--- controllers/people.js
+++ controllers/people.js
@@ -2,7 +2,6 @@ const peopleRouter = require('express').Router()
 const Person = require('../models/person')
 
 peopleRouter.get('/', async (req, res) => {
-  console.log('here')
   const people = await Person.find({})
   res.json(people)
 })
