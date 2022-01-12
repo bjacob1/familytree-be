e0eaa55abdc05ea7ae0e222a34c66c93b3e8e8eb
Brian Jacob <brianjacobk@gmail.com>
2022-01-12 00:11:20 -0500

    Get requests working

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const peopleRouter = require('./controllers/people')
 
-mongoose.connect(config.MONGO_URI)
+mongoose.connect(config.MONGO_URI).then(() => console.log('mongo connected'))
+
+app.use('/api/people/', peopleRouter)
+
+module.exports = app
--- controllers/people.js
+++ controllers/people.js
@@ -2,6 +2,7 @@ const peopleRouter = require('express').Router()
 const Person = require('../models/person')
 
 peopleRouter.get('/', async (req, res) => {
+  console.log('here')
   const people = await Person.find({})
   res.json(people)
 })
